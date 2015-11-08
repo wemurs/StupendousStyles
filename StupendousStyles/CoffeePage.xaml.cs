4c85853334f83b4ee3e7b20ddc6fa3cda064105e
wemurs <stuartwmurray@hotmail.com>
2015-11-08 22:42:12 +0000

    "coffee" page complete

--- StupendousStyles/CoffeePage.xaml.cs
+++ StupendousStyles/CoffeePage.xaml.cs
@@ -22,9 +22,58 @@ namespace StupendousStyles
     /// </summary>
     public sealed partial class CoffeePage : Page
     {
+        private string _Roast = "None";
+        private string _Sweetener = "None";
+        private string _Cream = "None";
+
         public CoffeePage()
         {
             this.InitializeComponent();
+        }
+
+        private void RoastClick(object sender, RoutedEventArgs e)
+        {
+            var selection = (MenuFlyoutItem)sender;
+            _Roast = selection.Text;
+            DisplayCoffee();
+        }
+
+        private void SweetenerClick(object sender, RoutedEventArgs e)
+        {
+            var selection = (MenuFlyoutItem)sender;
+            _Sweetener = selection.Text;
+            DisplayCoffee();
+        }
+
+        private void CreamClick(object sender, RoutedEventArgs e)
+        {
+            var selection = (MenuFlyoutItem)sender;
+            _Cream = selection.Text;
+            DisplayCoffee();
+        }
+
+        private void DisplayCoffee()
+        {
+            if (!_Roast.Equals("None"))
+            {
+                string Output = _Roast;
+
+                if (!_Sweetener.Equals("None"))
+                {
+                    Output = Output + " + " + _Sweetener;
+                }
+
+                if (!_Cream.Equals("None"))
+                {
+                    Output = Output + " + " + _Cream;
+                }
+
+                CoffeeDisplayTextBlock.Text = Output;
+            }
+            else
+            {
+                CoffeeDisplayTextBlock.Text = "";
+            }
         }
     }
 }
